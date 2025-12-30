7fff15e8db5f3b39e6c4e015db2fff1de6c2429d
gshyam46 <gshyam2603@gmail.com>
2025-12-30 18:58:27 +0530

    vercel prod

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
